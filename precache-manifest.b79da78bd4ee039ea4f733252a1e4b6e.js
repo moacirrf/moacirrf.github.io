c56a834bfc056a701aea155253eccfea264ec49b
Moacir da Roza Flores <moacirrf@gmail.com>
2022-11-01 22:51:55 -0300

    Updates

---
+++ precache-manifest.b79da78bd4ee039ea4f733252a1e4b6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e947ec9e4b6aafb9da5",
+    "url": "/moacirrf.github.io/static/css/main.c31d5ca6.chunk.css"
+  },
+  {
+    "revision": "9e947ec9e4b6aafb9da5",
+    "url": "/moacirrf.github.io/static/js/main.9e947ec9.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/moacirrf.github.io/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/moacirrf.github.io/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "290bece95a13151de114",
+    "url": "/moacirrf.github.io/static/js/runtime~main.290bece9.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/moacirrf.github.io/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6b5caf1c80662a40cc1f33e7ca5315a8",
+    "url": "/moacirrf.github.io/index.html"
+  }
+];
