575778d7f2ecd2d045e1453c761d43b8929748a1
Moacir da Roza Flores <moacirrf@gmail.com>
2022-11-01 23:16:04 -0300

    Alteração na index page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Em contrução
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Feito com React
         </a>
       </header>
     </div>
